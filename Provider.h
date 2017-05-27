c14293c04c4b60cb72ac95a7a8ed1e85f04700c2
Marcos khabie <marcos.khabie@ing.austral.edu.ar>
2017-05-27 14:53:29 -0300

    provider & register user

--- Provider.h
+++ Provider.h
@@ -4,5 +4,24 @@
 
 #ifndef EJERCICIO2_PROVIDER_H
 #define EJERCICIO2_PROVIDER_H
+typedef struct provider{
+
+    char* name;
+
+    int direction;
+    int tel;
+    int locality;
+    char* province;
+    char* country;
+    char* zipCode;
+    Product** productsToProvide;
+
+
+}Provider;
+
+Provider* createProvider(    int direction,int tel, int locality, char* province, char* country,char* zipCode, Product** productsToProvide);
+void provide(Market* market);
+
+void freeProvider(Provider* provider);
 
 #endif //EJERCICIO2_PROVIDER_H
--- RegisteredUser.h
+++ RegisteredUser.h
@@ -2,7 +2,31 @@
 // Created by Marcos khabie on 26/5/17.
 //
 
+#include <stringlist.h>
+
 #ifndef EJERCICIO2_REGISTEREDUSER_H
 #define EJERCICIO2_REGISTEREDUSER_H
+
+
+
+typedef struct registeredUser{
+
+    char* name;
+    int dni;
+    int direction;
+    int tel;
+    int locality;
+    char* province;
+    char* country;
+    char* zipCode;
+    int type ;// 0 client 1 admin
+    Sales** sales;
+
+}RegisteredUser;
+
+RegisteredUser* createUser(  char* name, int dni, int direction, int tel, int locality, char* province, char* country, char* zipCode, int type ,  Sales** sales);
+
+void freeUser(RegisteredUser* registeredUser);
+
 
 #endif //EJERCICIO2_REGISTEREDUSER_H
